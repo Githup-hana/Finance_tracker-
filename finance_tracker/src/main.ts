ec290717d6d6ba1c038201d07afb8da497712353
hana <hanaabrham89@gmail.com>
2025-02-17 10:23:43 +0100

    refactor transaction handling by removing counter functionality and implementing transaction storage in localStorage

--- finance_tracker/src/main.ts
+++ finance_tracker/src/main.ts
@@ -1,2 +1,36 @@
 import './style.css'
+interface Transaction {
+   
+    description: string;
+    amount: number;
+    type: "income"|"expense";
+  }
+  const form = document.querySelector("#transaction-form") as HTMLFormElement;
+  const typeInput = document.querySelector("#type-of-transactions") as HTMLSelectElement;
+  const amountInput = document.querySelector("#amount") as HTMLInputElement;
+  const descInput = document.querySelector("#description") as HTMLInputElement;
 
+
+  function getTransactions(): Transaction[] {
+    const data = localStorage.getItem("transactions");
+    return data ? JSON.parse(data) : [];
+  }
+
+  function savingTransaction() { 
+    const transactions: Transaction = {
+      description: descInput.value,
+      amount: parseFloat(amountInput.value),
+      type: typeInput.value as "income" | "expense"
+    };
+    const existingTransactions = getTransactions();
+    existingTransactions.push(transactions); 
+    const newTransaction=JSON.stringify(existingTransactions)
+    localStorage.setItem("transactions", newTransaction)
+    form.reset()
+  }
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    savingTransaction();
+});
